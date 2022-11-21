6e5798f6852ef848ab757edcb9f58e602fb9e2ec
Eeva-Kaarina Koukkari <Eeva-Kaarina.Koukkari@edu.savonia.fi>
2022-11-21 17:54:31 +0200

    muita muutoksia

--- app/src/main/java/com/example/tasktracker/MainActivity.kt
+++ app/src/main/java/com/example/tasktracker/MainActivity.kt
@@ -2,8 +2,8 @@ package com.example.tasktracker
 
 
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
-//import androidx.appcompat.app.AppCompatActivity
+//import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
